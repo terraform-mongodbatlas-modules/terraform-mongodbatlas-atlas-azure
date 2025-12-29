620446938c4931752ababedec798d3b6de9a3385
Espen Albert <espen.albert@mongodb.com>
2025-12-29 15:54:28 +0000

    chore rename example

--- examples/encryption_create_key_vault_private_networking/main.tf
+++ examples/encryption_create_key_vault_private_networking/main.tf
@@ -1,18 +1,22 @@
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
+resource "azurerm_resource_group" "this" {
+  location = var.azure_location
+  name     = var.resource_group_name
 }
 
 module "atlas_azure" {
-  source     = "../../"
-  project_id = var.project_id
+  source                   = "../../"
+  project_id               = var.project_id
+  service_principal_id     = var.service_principal_id
+  create_service_principal = var.create_service_principal
+  atlas_azure_app_id       = var.atlas_azure_app_id
 
   encryption = {
     enabled = true
     create_key_vault = {
       enabled                    = true
       name                       = var.key_vault_name
-      azure_location             = data.azurerm_resource_group.main.location
-      resource_group_name        = data.azurerm_resource_group.main.name
+      azure_location             = var.azure_location
+      resource_group_name        = azurerm_resource_group.this.name
       purge_protection_enabled   = var.purge_protection_enabled
       soft_delete_retention_days = var.soft_delete_retention_days
     }
--- examples/encryption_create_key_vault_private_networking/variables.tf
+++ examples/encryption_create_key_vault_private_networking/variables.tf
@@ -3,6 +3,12 @@ variable "project_id" {
   description = "MongoDB Atlas project ID"
 }
 
+variable "azure_location" {
+  type        = string
+  description = "Azure location"
+  default     = "eastus"
+}
+
 variable "subscription_id" {
   type        = string
   description = "Azure subscription ID"
@@ -41,4 +47,22 @@ variable "private_endpoint_regions" {
   type        = set(string)
   default     = []
   description = "Atlas regions for private endpoints (Atlas format: US_EAST_2, EUROPE_WEST). Required when require_private_networking = true."
+}
+
+variable "atlas_azure_app_id" {
+  type        = string
+  description = "MongoDB Atlas Azure application ID. This is the application ID registered in Azure AD for MongoDB Atlas."
+  default     = "9f2deb0d-be22-4524-a403-df531868bac0"
+}
+
+variable "create_service_principal" {
+  type        = bool
+  default     = true
+  description = "Create Azure AD service principal. Set false and provide service_principal_id for existing."
+}
+
+variable "service_principal_id" {
+  type        = string
+  default     = null
+  description = "Existing service principal object ID. Required if create_service_principal = false."
 }
