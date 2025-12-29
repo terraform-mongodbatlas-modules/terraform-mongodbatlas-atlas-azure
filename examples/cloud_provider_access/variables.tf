2dd57c26348be4bc6f2467a96f6a36a79e3dec20
Espen Albert <espen.albert@mongodb.com>
2025-12-29 10:52:45 +0000

    update description

--- examples/cloud_provider_access/variables.tf
+++ examples/cloud_provider_access/variables.tf
@@ -12,7 +12,7 @@ variable "subscription_id" {
 
 variable "atlas_azure_app_id" {
   type        = string
-  description = "MongoDB Atlas Azure application ID"
+  description = "MongoDB Atlas Azure application ID. This is the application ID registered in Azure AD for MongoDB Atlas."
   default     = "9f2deb0d-be22-4524-a403-df531868bac0"
 }
 
