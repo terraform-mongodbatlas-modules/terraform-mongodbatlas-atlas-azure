7dfa2fd610ecbbb0db061c3e6a034b3272909c2b
Espen Albert <espen.albert@mongodb.com>
2026-04-27 20:04:23 +0100

    chore: bumps versions

--- examples/encryption_create_key_vault_private_networking/versions.tf
+++ examples/encryption_create_key_vault_private_networking/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = ">= 2.0"
+      version = ">= 2.8"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = ">= 4.42"
     }
     azuread = {
       source  = "hashicorp/azuread"
